059db0262ab09eea1a32addb98b21759bef525fe
GKnico <74272139+GKnico@users.noreply.github.com>
2022-03-14 17:52:27 -0700

    pageTable.h

--- pageTable.h
+++ pageTable.h
@@ -1,29 +1,23 @@
 #ifndef PAGETABLE_H
 #define PAGETABLE_H
 
-#define CACHE_TLB 0
-
 #include <stdlib.h>
-
 typedef struct pageTable{ 
-    unsigned int depth = 0;
-    int *levelCount; 
-    int bitShift[levelCount];
-    int bitMask[];
-    int entryCount[];
+    unsigned int levelCount;  
+    unsigned int bitmask[levelCount];
+    unsigned int bitShift;
+    unsigned int entryCount;
 };
 
-typedef struct Level{
-    Level* nextLevel[];
-    int map[];
+typedef struct level{
+    pageTable *pageTablePtr;
+    unsigned int depth;
+    level *nextLevelPtr[levelCount];
+
+    unsigned int map[];
 };
 
-
-void pageInsert(pageTable *pagetable, unsigned int virtualAddress, unsigned int frame);
-Map* pageLookup(pageTable *pagetable, unsigned int virtualAddress);
-unsigned int virtualAddressToPageNum (unsigned int virtualAddress, unsigned int mask, unsigned int shift); 
-
-
+pageTable pageInsert(pageTablePtr, address, frame);
 
 
 
